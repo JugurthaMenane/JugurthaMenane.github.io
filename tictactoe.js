4bbe4d35de9f2e9ed057fdbc8d100594cd90061b
JugurthaMenane <44397536+JugurthaMenane@users.noreply.github.com>
2018-11-23 17:18:41 +0100

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,53 +1,42 @@
-    // alert('hello')
-
-
-function Initialisation() {
-    //On cache le bouton "Rejouer"
-    document.getElementById("replay").style.visibility = "hidden";
-    
-}
-//On définit les players
-    const players = ['Red', 'Blue'];
+ const players = ['Red', 'Blue'];
     //Et le player qui commence
     let player = players[0];
-
+    // let boxPlayed = 0;
 
 function play(zone) { //Recupère le nom de la zone cliquée via "zone"
-    //Si le player et egale au player 'X'
+    
     if (player == players[0]) {
         if (document.getElementById(zone).className.indexOf('filled') >= 0) {
             //On affiche une alerte
             alert("You can't do that...");
         } else {
-            //Sinon on definit le background de la case avec l'image Croix
+            
             document.getElementById(zone).style.backgroundColor = "red";
             //Et on lui ajoute la class "filled" pour ne plus pouvoir la changer
             document.getElementById(zone).className += " filled";
             document.getElementById(zone).textContent = "  ";
-            //On passe la main au player 'RONDS'
+            //On passe la main au player 'Blue'
             player = players[1];
         }
     }
-    else if (player === players[1]) { //Si le player et egale au player 'O'
+    else if (player === players[1]) { 
         //Si la case et dejà pleine (class filled)
         if (document.getElementById(zone).className.indexOf('filled') >= 0) {
             //On affiche un alerte
             alert("This case is already filled");
         } else {
-            //Sinon on definit le background de la case avec l'image Rond
             document.getElementById(zone).style.backgroundColor = "blue";
             //Et on lui ajoute la class "filled" pour ne plus pouvoir la changer
             document.getElementById(zone).className += " filled";
             document.getElementById(zone).textContent = " ";
-            //On passe la main au player 'CROIX'
+            //On passe la main au player 'Red'
             player = players[0];
         }
+        
     }
-    ;
-    //On fait un check :)
     checking();
 }
-;
+
 function checking() {
     //Definition des getelement pour racourcir le if()
     one = document.getElementById('Cell0').textContent;
@@ -73,10 +62,7 @@ function checking() {
         } else if (player === players[1]) {
             alert('Player Red WIN');
         }
-        //Rend visible le bouton rejouer
-        document.getElementById("replay").style.visibility = "visible";
-        document.getElementById("replay").onclick = function() {
-            location.reload();
-        }
-    }
+        
+document.getElementById("replay").onclick = function() {
+    location.reload();
 }
